c52fdc5f7ce7636bc32ae4103747b75a3e33958b
Kelvin9393 <kelvinlssiang9393@hotmail.com>
2017-10-06 22:20:21 +0800

    first commit

---
+++ FaceIt/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by KELVIN LING SHENG SIANG on 16/09/2017.
+//  Copyright © 2017 KELVIN LING SHENG SIANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    var expression = FacialExpression(eyes: .closed, mouth: .frown) {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        switch expression.eyes {
+        case .open:
+            faceView?.eyesOpen = true
+        case .closed:
+            faceView?.eyesOpen = false
+        case .squinting:
+            faceView?.eyesOpen = false
+        }
+        
+        faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+    }
+    
+    private let mouthCurvatures = [FacialExpression.Mouth.frown: -1.0, .smirk: -0.5, .neutral: 0.0, .grin: 0.5, .smile: 1.0]
+
+
+}
+
